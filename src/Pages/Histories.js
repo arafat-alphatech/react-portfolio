aff8819b66af61e3d08ad909cc722dca7f56b18f
arafat <arafat@alphatech.id>
2018-11-19 09:11:25 +0700

    history page

--- src/Pages/Histories.js
+++ src/Pages/Histories.js
@@ -47,38 +47,46 @@ class Histories extends Component {
                                     <div className="card-body">
                                         <h4 className="text-center">Data History Belanja Anda</h4>
                                         <hr/>
+                                    </div>
+                                </div>
                                         {
                                             datas.map((item, key) => {
                                                 return (
                                                     <div key={key}>
-                                                        <h5>Waktu: {item.updatedAt}</h5>
+                                                        <div className="card mt-3 mb-3">
+                                                            <div className="card-body">
+                                                        <h5>Waktu transaksi: {item.updatedAt}</h5>
+                                                        <hr/>
                                                         <h6>Total Buku: {item.total_qty}</h6>
                                                         <h6>Total Biaya: Rp. {item.total_price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</h6>
                                                         <h5>Barang-barang yang di beli:</h5>
-                                                        {
-                                                            item.datas.map((i, k) => {
-                                                                return (
-                                                                    <div className="row" key={k}>
-                                                                        <div className="col-sm-6">
-                                                                            {i['buku.judul']}
+                                                            {
+                                                                item.datas.map((i, k) => {
+                                                                    return (
+                                                                        <div className="row" key={k}>
+                                                                            <div className="col-sm-6">
+                                                                                <Link to={"detail/" + i['buku.id']}>
+                                                                                    {i['buku.judul']}
+                                                                                </Link>
+                                                                                
+                                                                            </div>
+                                                                            <div className="col-sm-3 text-center">
+                                                                                {i.qty} &nbsp;
+                                                                            </div>
+                                                                            <div className="col-sm-3">
+                                                                                Rp {parseInt(i.price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+                                                                            </div>
                                                                         </div>
-                                                                        <div className="col-sm-3 text-center">
-                                                                            {i.qty} &nbsp;
-                                                                        </div>
-                                                                        <div className="col-sm-3">
-                                                                            Rp {parseInt(i.price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
-                                                                        </div>
-                                                                    </div>
-                                                                    )
-                                                            })
-                                                        }
-                                                        <br/>
+                                                                        )
+                                                                    })
+                                                                }
+                                                            <hr/>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 )
                                             })
                                         }
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -18,6 +18,7 @@ class Home extends Component {
     }
     
     render() {
+        
         const listBooks = this.props.listBooks
         return (
             <div >
